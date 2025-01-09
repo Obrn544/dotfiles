1a2a962a02610fe5f80dff9fd27823a8abdf25cc
Omar Obregon <oobregon544@gmail.com>
2025-01-09 17:49:13 -0500

    update Silvia theme configuration: adjust border width, modify corner radius, and change launcher label icon color

--- config/bspwm/rices/silvia/Theme.sh
+++ config/bspwm/rices/silvia/Theme.sh
@@ -18,7 +18,7 @@ read -r RICE < "$HOME"/.config/bspwm/.rice
 
 # Vars config for Silvia Rice
 # Bspwm border		# Fade true|false	# Shadows true|false	# Corner radius		# Shadow color
-BORDER_WIDTH="0"	P_FADE="true"		P_SHADOWS="true"		P_CORNER_R="6"		SHADOW_C="#000000"
+BORDER_WIDTH="3"	P_FADE="true"		P_SHADOWS="true"		P_CORNER_R="16"		SHADOW_C="#000000"
 
 # (Gruvbox) colorscheme
 bg="#282828"  fg="#fbf1c7"
@@ -28,10 +28,6 @@ blackb="#a89984"  redb="#fb4934"  greenb="#b8bb26"  yellowb="#fabd2f"
 
 blue="#458588"   magenta="#b16286"   cyan="#689d6a"   white="#ebdbb2"
 blueb="#83a598"  magentab="#d3869b"  cyanb="#8ec07c"  whiteb="#ebdbb2"
-
-# Gtk theme vars
-gtk_theme="Gruvbox-zk"	gtk_icons="Gruvbox-Plus-Dark"	gtk_cursor="Qogirr-Dark"	geany_theme="z0mbi3-Gruvbox"
-
 
 # Set bspwm configuration
 set_bspwm_config() {
@@ -233,38 +229,6 @@ set_launchers() {
 EOF
 }
 
-set_appearance() {
-	# Set the gtk theme corresponding to rice
-	if pidof -q xsettingsd; then
-		sed -i "$HOME"/.config/bspwm/xsettingsd \
-			-e "s|Net/ThemeName .*|Net/ThemeName \"$gtk_theme\"|" \
-			-e "s|Net/IconThemeName .*|Net/IconThemeName \"$gtk_icons\"|" \
-			-e "s|Gtk/CursorThemeName .*|Gtk/CursorThemeName \"$gtk_cursor\"|"
-	else
-		sed -i "$HOME"/.config/gtk-3.0/settings.ini \
-			-e "s/gtk-theme-name=.*/gtk-theme-name=$gtk_theme/" \
-			-e "s/gtk-icon-theme-name=.*/gtk-icon-theme-name=$gtk_icons/" \
-			-e "s/gtk-cursor-theme-name=.*/gtk-cursor-theme-name=$gtk_cursor/"
-
-		sed -i "$HOME"/.gtkrc-2.0 \
-			-e "s/gtk-theme-name=.*/gtk-theme-name=\"$gtk_theme\"/" \
-			-e "s/gtk-icon-theme-name=.*/gtk-icon-theme-name=\"$gtk_icons\"/" \
-			-e "s/gtk-cursor-theme-name=.*/gtk-cursor-theme-name=\"$gtk_cursor\"/"
-	fi
-
-	sed -i -e "s/Inherits=.*/Inherits=$gtk_cursor/" "$HOME"/.icons/default/index.theme
-
-	# Reload daemon and apply gtk theme
-	pidof -q xsettingsd && killall -HUP xsettingsd
-	xsetroot -cursor_name left_ptr
-}
-
-# Apply Geany Theme
-set_geany(){
-	sed -i ${HOME}/.config/geany/geany.conf \
-	-e "s/color_scheme=.*/color_scheme=$geany_theme.conf/g"
-}
-
 # Launch theme
 launch_theme() {
 
@@ -289,6 +253,4 @@ set_picom_config
 set_dunst_config
 set_eww_colors
 set_launchers
-set_appearance
-set_geany
 launch_theme
