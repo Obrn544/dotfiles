f5248bdd9c993735739a7dee29eebe0c326a2081
Omar Obregon <oobregon544@gmail.com>
2025-01-20 17:09:17 -0500

    update author attribution to obrn544 in various scripts and configurations

--- config/bspwm/src/spotify.sh
+++ config/bspwm/src/spotify.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# ███████╗██████╗  ██████╗ ████████╗██╗███████╗██╗   ██╗
+# ██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝██║██╔════╝╚██╗ ██╔╝
+# ███████╗██████╔╝██║   ██║   ██║   ██║█████╗   ╚████╔╝ 
+# ╚════██║██╔═══╝ ██║   ██║   ██║   ██║██╔══╝    ╚██╔╝  
+# ███████║██║     ╚██████╔╝   ██║   ██║██║        ██║   
+# ╚══════╝╚═╝      ╚═════╝    ╚═╝   ╚═╝╚═╝        ╚═╝   
+
+#   Author: obrn544
+#   url: https://github.com/obrn544/dotfiles
+#   Script to display current song playing in spotify
 
 if playerctl --player=spotify status > /dev/null 2>&1; then
     playerctl --player=spotify metadata --format '{{ title }}'
